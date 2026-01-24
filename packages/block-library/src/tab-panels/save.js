b1870dd57d4c922e928b46ffb76c219336417acf
Seth Rubenstein <sethrubenstein@users.noreply.github.com>
2026-01-23 20:16:14 -0500

    Feat/core tabs restructure (#74412)
    * New, reworked structure and editing
    
    * Refactoring the entire tabs experience, rough right now, should have this cleaned up by end of week.
    
    * Greater granular flex styling, need to go back in and cleanup old styles
    
    * Full border styles for menu items
    
    * Style tweaks
    
    * Enhance tab functionality by updating the AddTabToolbarControl to manage tabs-menu selection and active tab index. Implement auto-edit mode for newly added tabs in the TabsMenu edit component.
    
    * Allow renaming of tab blocks, leaving note in deprecation upgrade to remove later
    
    * Style and dev cleanup
    
    * Default layouts for tabs-menu and keyboard handling
    
    * Tweaks
    
    * PHPCS cleanup
    
    * Dimensions block.json supports, correct format
    
    * Adding shadow support
    
    * Cleanup
    
    * Increased performance, move client side and server side tabs list generation all into context.
    
    * Cleanup
    
    * Cleanup on styles and blockgap
    
    * Lets back off on blockspacing for tabs-menu for now
    
    * Introduce new core/tabs-menu-item block (#14)
    
    * New block/core tabs menu item (#16)
    
    * Introducing new core/tabs-menu-item block, utilizes blockcontextprovider hack we use for templating blocks. *May rename the block to tabs-menu-item-template
    
    * Trying to get this working as I'd like
    
    * Introducing core/tabs-menu-item, a templating block for styling the interior element of tabs-menu
    
    * Style cleanup
    
    * Block cleanup for MVP
    
    * Add text align to tabs-menu-item
    
    * Now tabs-menu interior items can be styled using dimensions controls in tabs-menu
    
    * Cleanup
    
    * Remove refactor artifact
    
    * Cleanup and initializing new tab with a proper label
    
    * Tab removal button
    
    - Add "Remove Tab" toolbar button to tabs block
    - Code cleanup
    
    * Clearer labels on block toolbar buttons
    
    * Cleaning up tabs id, utilizing anchor instead of a custom attribute, injecting tabs id dynamically if none present early in core/tabs context.
    
    * new add tab controls per some internal prc user testing with editors
    
    * Leaving the add tab in place in tabs-menu-item, pulling from tabs-menu extra appender... for now
    
    * Cleanup, align with labels like core/accordion
    
    * Move add and remove tab into segmented block toolbar group
    
    * Sensible defaults for border on tabs-menu
    
    * Resolve phpcs issue
    
    * Cleanup on style
    
    * Introduce custom controls for moving tab
    
    * remove unused tabs styles, get rid of opacity on styles, simplify styles further, enhance block example.
    
    * Cleanup on block mover
    
    * Resolve some oustanding linting issues
    
    * Linting error resolution
    
    * Resolving scss linting issue
    
    * Update core blocks documentation to reflect changes in supported features for tabs and tab panels, including color and typography attributes. Hoping this resolves final PR checks.
    
    Co-authored-by: sethrubenstein <smrubenstein@git.wordpress.org>
    Co-authored-by: mikachan <mikachan@git.wordpress.org>
    Co-authored-by: annezazu <annezazu@git.wordpress.org>

--- packages/block-library/src/tab-panels/save.js
+++ packages/block-library/src/tab-panels/save.js
@@ -3,13 +3,9 @@
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
-export default function save( { attributes } ) {
-	const { anchor } = attributes;
-
-	const tabPanelId = anchor;
-
+export default function save() {
 	const blockProps = useBlockProps.save();
 	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
 
-	return <section { ...innerBlocksProps } id={ tabPanelId } />;
+	return <div { ...innerBlocksProps } />;
 }
